1f8bf368dddaba9d93cc65e756a3275100cdda2b
Vikasez4u <Vikasez4u@gmail.com>
2025-07-09 12:00:53 +0530

    code changes

--- src/authConfig.js
+++ src/authConfig.js
@@ -11,8 +11,8 @@ export const msalConfig = {
     auth: {
         clientId: "18bea863-348d-41f2-b82b-6162e1822bbb",
     authority: "https://login.microsoftonline.com/6eb54db1-fc6e-4b0a-a00b-930182dca624",
-    //redirectUri: "https://g2g-chatbot-geakehf4aqamfcfb.eastasia-01.azurewebsites.net",
-    redirectUri: "http://localhost:5173", // This is the base URL of your application where MSAL will redirect after authentication
+    redirectUri: "https://g2g-chatbot-geakehf4aqamfcfb.eastasia-01.azurewebsites.net",
+    //redirectUri: "http://localhost:3000", // This is the base URL of your application where MSAL will redirect after authentication
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
