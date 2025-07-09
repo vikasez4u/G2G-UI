902766eae25d3601c2c6c5a8b5eecd8e48d0a6b0
Vikasez4u <Vikasez4u@gmail.com>
2025-07-09 20:21:49 +0530

    Code modified for sso

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import MsalLoader from "./MsalLoader";
 import "./index.css";
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MsalLoader />
+    <App />
   </React.StrictMode>
 );
+
